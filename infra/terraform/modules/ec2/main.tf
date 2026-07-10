43f6bc7f15a7d7f187beff3cc7544d817e747d74
MuhammadZaid11 <zaid.anwer.dev@gmail.com>
2026-07-10 22:57:01 +0500

    infra debugging

--- infra/terraform/modules/ec2/main.tf
+++ infra/terraform/modules/ec2/main.tf
@@ -1,14 +1,51 @@
-resource "aws_vpc" "main" {
+﻿resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
 
-  cidr_block = "0.0.0.0/0"
-
+  tags = {
+    Name = "devops-vpc"
+  }
 }
 
+resource "aws_subnet" "main" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "devops-subnet"
+  }
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "devops-igw"
+  }
+}
+
+resource "aws_route_table" "main" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name = "devops-rt"
+  }
+}
+
+resource "aws_route_table_association" "main" {
+  subnet_id      = aws_subnet.main.id
+  route_table_id = aws_route_table.main.id
+}
 
 resource "aws_security_group" "ansible_sg" {
   name        = "ansible-security-group"
   description = "Allow SSH and HTTP traffic"
-  vpc_id = aws_vpc.main.id
+  vpc_id      = aws_vpc.main.id
 
   ingress {
     description = "SSH Access"
@@ -44,8 +81,9 @@ resource "aws_instance" "ansible_server" {
   ami                    = var.ami
   instance_type          = var.instance_type
   key_name               = "Backup_key"
+  subnet_id              = aws_subnet.main.id
   vpc_security_group_ids = [aws_security_group.ansible_sg.id]
   tags = {
-    Name = "worker-${count.index + 1}"
+    Name = "${var.name}-${count.index + 1}"
   }
 }
--- infra/terraform/modules/ec2/output.tf
+++ infra/terraform/modules/ec2/output.tf
@@ -1,12 +1,12 @@
 output "aws_instance_ip" {
-  value = aws_instance.Ansible-server[*].public_ip
+  value = aws_instance.ansible_server[*].public_ip
 }
 
 output "aws_instance_id" {
-  value = aws_instance.Ansible-server[*].id
+  value = aws_instance.ansible_server[*].id
 }
 
 output "worker_nodes_ips" {
   description = "Public IPs of worker nodes"
-  value       = aws_instance.Ansible-server[*].public_ip
+  value       = aws_instance.ansible_server[*].public_ip
 }
